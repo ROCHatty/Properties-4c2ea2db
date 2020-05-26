70bb4a4e8bcf78bd42831e8ded7f0377a92ac0a5
Aaron Jonk <2119311@talnet.nl>
2020-05-26 13:02:03 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -2,6 +2,8 @@
 
 class MyLogger
 {
+	private $origin;
+	
 	private function logWithTime()
 	{
 		echo "[" . date('Y-m-d H:i:s') . "] ";
@@ -9,7 +11,7 @@ class MyLogger
 	
 	private function log($message, $level)
 	{
-		echo strtoupper($level) . ": " . $message . PHP_EOL;
+		echo $this -> origin . " - " . strtoupper($level) . ": " . $message . PHP_EOL;
 	}
 	
 	public function warning($message)
@@ -33,12 +35,18 @@ class MyLogger
 	{
 		$this -> log($message, "debug");
 	}
+	
+	public function setOrigin($origin)
+	{
+		$this -> origin = $origin;
+	}
 }
 
 $logs = new MyLogger();
-$logs -> warning("Dit is een warning");
-$logs -> error("Dit is een error");
-$logs -> info("Dit is wat info");
-$logs -> debug("Dit is een debug message");
+$logs -> setOrigin('TestClass');
+$logs -> warning("Dit is een waarschuwing");
+$logs -> error("Dit is een fout");
+$logs -> info("Dit is wat informatie");
+$logs -> debug("Dit is een debug bericht");
 
 ?>
